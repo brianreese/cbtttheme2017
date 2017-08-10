f70b19c1514d03d63420ec8be67b1aa94ac1e321
Brian Reese <Brian_R_Reese@oa.eop.gov>
2017-08-10 09:12:47 -0400

    Copy theme files from zip archive.

---
+++ js/menuDropdown.js
@@ -0,0 +1,13 @@
+
+		jQuery(document).ready(function($) {
+			jQuery('.stellarnav').stellarNav({
+				theme: 'light'
+			});
+		});
+
+
+      jQuery(document).ready(function ($) {
+      jQuery('[data-toggle="offcanvas"]').click(function () {
+        jQuery('.row-offcanvas').toggleClass('active')
+      });
+    });
